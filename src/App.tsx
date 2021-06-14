f8a6d3e39bb18b8f7e11e11aec0d2c1ce3b6ffbc
VladSemenik <vlad.semenik@gmail.com>
2021-06-14 17:25:02 +0300

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import indigo from '@material-ui/core/colors/indigo';
+import pink from '@material-ui/core/colors/pink';
+
+import './App.css';
+import DwvComponent from './DwvComponent';
+
+const theme = createMuiTheme({
+  typography: {
+    useNextVariants: true,
+  },
+  palette: {
+    primary: indigo,
+    secondary: pink,
+    type: 'light'
+  }
+});
+
+function App() {
+  return (
+    <MuiThemeProvider theme={theme}>
+      <CssBaseline />
+      <div className="App">
+        <DwvComponent />
+      </div>
+    </MuiThemeProvider>
+  );
+}
+
+export default App;
